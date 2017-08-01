42c8b60c9db6df0196b67156e9254a1424b4d5a9
FordLambert <ford.lambert@protonmail.ch>
2017-08-01 17:42:20 +0200

    cleaning code
    html and css had been cleaned a bit, spaces removing or adding,
    comments...

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,13 @@
 // custom  jquery code //
 $(function () {
+
     // alert popup if submit on the contact or movies page//
     if ($(".contact-page" ).length || $(".prog-page" ).length) {
         document.addEventListener("submit", function() {
             alert("Message envoyé avec succès !");
         });
     }
+
     // Better looking checkbox, credit to travislaynewilson //
     if ($(".prog-page" ).length) {
         $('.button-checkbox').each(function () {
@@ -22,6 +24,7 @@ $(function () {
                         icon: 'glyphicon glyphicon-unchecked'
                     }
                 };
+
             // Event Handlers
             $button.on('click', function () {
                 $checkbox.prop('checked', !$checkbox.is(':checked'));
@@ -31,6 +34,7 @@ $(function () {
             $checkbox.on('change', function () {
                 updateDisplay();
             });
+
             // Actions
             function updateDisplay() {
                 var isChecked = $checkbox.is(':checked');
@@ -52,6 +56,7 @@ $(function () {
                         .addClass('btn-default');
                 }
             }
+            
             // Initialization
             function init() {
                 updateDisplay();
@@ -63,28 +68,32 @@ $(function () {
             init();
         });
     }
+
     // Progress bar on article page only //
     if ($(".article-page" ).length) {
+
         function updateProgress(num1, num2){
           var percent = Math.ceil( num1 / num2 * 100 ) + '%';
-          document.getElementById('progress').style.width = percent;
+          document.getElementById('progress-bar').style.width = percent;
         }
+
         window.addEventListener('scroll', function(){
           var top = window.scrollY;
           var height = document.body.getBoundingClientRect().height - window.innerHeight;
           updateProgress(top, height);
         });
     }
+
     //checking on movie page if we have a "day" parameter and open the right tab//
     if ($(".prog-page" ).length) {
-        if(window.location.href.indexOf("#day2") > -1) {
+        if(window.location.href.indexOf("#day") > -1) {
             $("#day1").prop("checked", "false");
+        }
+        if(window.location.href.indexOf("#day2") > -1) {
             $("#day2").prop("checked", "true");
         } else if (window.location.href.indexOf("#day3") > -1) {
-            $("#day1").prop("checked", "false");
             $("#day3").prop("checked", "true");
         } else if (window.location.href.indexOf("#day4") > -1) {
-            $("#day1").prop("checked", "false");
             $("#day4").prop("checked", "true");
         }
     }
