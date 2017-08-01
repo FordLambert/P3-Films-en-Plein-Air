e8ee3def9665be28dc39fcc1fb41992ecca50cdd
FordLambert <ford.lambert@protonmail.ch>
2017-08-01 10:52:14 +0200

    js cleaning
    custom.js had been cleaned, fucntions are now active on a specific page
    only

--- js/custom.js
+++ js/custom.js
@@ -1,84 +1,78 @@
-
-document.addEventListener("submit", function() {
-	alert("Message envoyé avec succès !");
-});
-
-/*----- Better looking checkbox, credit to travislaynewilson -----*/
+// custom  jquery code //
 $(function () {
-    $('.button-checkbox').each(function () {
-        // Settings
-        var $widget = $(this),
-            $button = $widget.find('button'),
-            $checkbox = $widget.find('input:checkbox'),
-            color = $button.data('color'),
-            settings = {
-                on: {
-                    icon: 'glyphicon glyphicon-check'
-                },
-                off: {
-                    icon: 'glyphicon glyphicon-unchecked'
+    // alert popup if submit on the contact or movies page//
+    if ($(".contact-page" ).length || $(".prog-page" ).length) {
+        document.addEventListener("submit", function() {
+            alert("Message envoyé avec succès !");
+        });
+    }
+    // Better looking checkbox, credit to travislaynewilson //
+    if ($(".prog-page" ).length) {
+        $('.button-checkbox').each(function () {
+            // Settings
+            var $widget = $(this),
+                $button = $widget.find('button'),
+                $checkbox = $widget.find('input:checkbox'),
+                color = $button.data('color'),
+                settings = {
+                    on: {
+                        icon: 'glyphicon glyphicon-check'
+                    },
+                    off: {
+                        icon: 'glyphicon glyphicon-unchecked'
+                    }
+                };
+            // Event Handlers
+            $button.on('click', function () {
+                $checkbox.prop('checked', !$checkbox.is(':checked'));
+                $checkbox.triggerHandler('change');
+                updateDisplay();
+            });
+            $checkbox.on('change', function () {
+                updateDisplay();
+            });
+            // Actions
+            function updateDisplay() {
+                var isChecked = $checkbox.is(':checked');
+                // Set the button's state
+                $button.data('state', (isChecked) ? "on" : "off");
+                // Set the button's icon
+                $button.find('.state-icon')
+                    .removeClass()
+                    .addClass('state-icon ' + settings[$button.data('state')].icon);
+                // Update the button's color
+                if (isChecked) {
+                    $button
+                        .removeClass('btn-default')
+                        .addClass('btn-' + color + ' active');
                 }
-            };
-        // Event Handlers
-        $button.on('click', function () {
-            $checkbox.prop('checked', !$checkbox.is(':checked'));
-            $checkbox.triggerHandler('change');
-            updateDisplay();
+                else {
+                    $button
+                        .removeClass('btn-' + color + ' active')
+                        .addClass('btn-default');
+                }
+            }
+            // Initialization
+            function init() {
+                updateDisplay();
+                // Inject the icon if applicable
+                if ($button.find('.state-icon').length == 0) {
+                    $button.prepend('<i class="state-icon ' + settings[$button.data('state')].icon + '"></i> ');
+                }
+            }
+            init();
         });
-        $checkbox.on('change', function () {
-            updateDisplay();
+    }
+    // Progress bar on article page only //
+    if ($(".article-page" ).length) {
+        function updateProgress(num1, num2){
+          var percent = Math.ceil( num1 / num2 * 100 ) + '%';
+          document.getElementById('progress').style.width = percent;
+        }
+        window.addEventListener('scroll', function(){
+          var top = window.scrollY;
+          var height = document.body.getBoundingClientRect().height - window.innerHeight;
+          updateProgress(top, height);
         });
-        // Actions
-        function updateDisplay() {
-            var isChecked = $checkbox.is(':checked');
-            // Set the button's state
-            $button.data('state', (isChecked) ? "on" : "off");
-            // Set the button's icon
-            $button.find('.state-icon')
-                .removeClass()
-                .addClass('state-icon ' + settings[$button.data('state')].icon);
-            // Update the button's color
-            if (isChecked) {
-                $button
-                    .removeClass('btn-default')
-                    .addClass('btn-' + color + ' active');
-            }
-            else {
-                $button
-                    .removeClass('btn-' + color + ' active')
-                    .addClass('btn-default');
-            }
-        }
-        // Initialization
-        function init() {
-            updateDisplay();
-            // Inject the icon if applicable
-            if ($button.find('.state-icon').length == 0) {
-                $button.prepend('<i class="state-icon ' + settings[$button.data('state')].icon + '"></i> ');
-            }
-        }
-        init();
-    });
+    }
 });
-/*-----Progress bar-----*/
-function updateProgress(num1, num2){
-  var percent = Math.ceil( num1 / num2 * 100 ) + '%';
-  document.getElementById('progress').style.width = percent;
-}
-
-window.addEventListener('scroll', function(){
-  var top = window.scrollY;
-  var height = document.body.getBoundingClientRect().height - window.innerHeight;
-  updateProgress(top, height);
-});
-
-
-
-
-/* if ($(".article-page" ).length) {         
-function updateProgress(num1, num2){             
-var percent = Math.ceil( num1 / num2 * 100 ) + '%';            
- document.getElementById('progress').style.width = percent;         }       
-    window.addEventListener('scroll', function(){             var top = window.scrollY;    
-             var height = document.body.getBoundingClientRect().height - window.innerHeight;
-             updateProgress(top, height);         });     }/*
